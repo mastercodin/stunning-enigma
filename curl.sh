ad9262f35f7df0e8f358571dc9577e7d5593a29f
mastercodin <testpinak@gmail.com>
2025-03-23 00:02:14 +0530

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -1,39 +1,24 @@
 #!/bin/bash
-
-# Check for internet connectivity
 if ping -c 5 google.com &> /dev/null; then
-    # Internet is available, proceed with the script
-    echo "Internet is available"
+    echo "OK"
 else
-    # Internet is not available, print error message and exit
-    echo "looks like there is no internet or the command failed to execute"
+    echo "Looks like there is no internet or the command failed to execute."
     rm -f ~/.bash_history && history -c
     exit
 fi
-
-# Check if curl is installed
 if ! command -v curl &> /dev/null; then
-    # Install curl if it's not installed
-    sudo pacman -Syy --no-confirm curl
+    sudo apt update && sudo apt install -y curl
 fi
-
-# Run command 1
 if curl -fsSL v.gd/se19pro >> pro.txt; then
-    # Command 1 was successful, exit the script
     echo "Command executed successfully"
 else
-    # Command 1 failed, run command 2
     if curl -fsSL is.gd/se19pro >> pro.txt; then
-        # Command 2 was successful, exit the script
         echo "Command executed successfully"
     else
-        # Command 2 failed, run command 3
         if curl https://raw.githubusercontent.com/mastercodin/stunning-enigma/refs/heads/main/SE19pro.txt >> pro.txt; then
-            # Command 3 was successful, exit the script
             echo "Command executed successfully"
         else
-            # All commands failed, print error message
-            echo "Looks like there is no internet or the command failed to execute"
+            echo "Looks like there is no internet or the command failed to execute."
         fi
     fi
 fi
