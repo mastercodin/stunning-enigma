2667a09fb41981ba0891e9e08bbd66f213d0eb9d
mastercodin <testpinak@gmail.com>
2025-10-05 23:07:31 +0530

    Create chat.bash

---
+++ chat.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+cat <<'EOF' >> ~/.bashrc
+function chat() {
+    local response
+    response=$(curl -s ch.at/v1/chat/completions --data "{\"messages\":[{\"role\":\"user\",\"content\":\"\$1\"}]}" | jq -r '.choices[0].message.content')
+    echo "\$response"
+}
+EOF
